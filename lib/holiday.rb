f396f29d1554f831381f74ef7d0fed751957a350
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-19 16:23:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,21 +73,3 @@ end
 
 
 
-
-puts add_new_holiday_with_supplies(holiday_supplies = {
-  :winter => {
-    :christmas => ["Lights", "Wreath"],
-    :new_years => ["Party Hats"]
-  },
-  :summer => {
-    :fourth_of_july => ["Fireworks", "BBQ"]
-  },
-  :fall => {
-    :thanksgiving => ["Turkey"]
-  },
-  :spring => {
-    :memorial_day => ["BBQ"]
-  }
-}, )
-
-
